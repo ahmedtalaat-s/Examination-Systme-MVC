bbb4843c8998bbbe97092374ce865f424cae5c2f
Hamdy <hamdymohamed2651@gmail.com>
2025-10-04 18:36:32 +0300

    somehting

---
+++ Domains/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+    public class Subject
+    {
+        [Key]
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
