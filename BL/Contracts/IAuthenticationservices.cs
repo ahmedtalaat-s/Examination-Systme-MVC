6ec1abacf4b0624e97069b15b7b16cad7012c850
ahmedtalat-s <ahmedtalaatz102@gmail.com>
2025-10-06 11:56:04 +0300

    Add authrization in BL

---
+++ BL/Contracts/IAuthenticationservices.cs
@@ -0,0 +1,18 @@
+﻿using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Domains;
+
+namespace BL.Services;
+
+public interface IAuthenticationservices
+{
+    User login(string email, string password);
+    (bool status, string messageError)  register(User user, List<int> subjects);
+
+}
